5861e5db459c2505bf86aad07d934bddb8776fef
Carlos Mancini <carlos@devsquad.email>
2024-01-11 14:58:42 -0300

    First commit

---
+++ src/main/java/com/br/datasig/datasigpdvapi/DatasigPdvApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.datasig.datasigpdvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasigPdvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasigPdvApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/datasig/datasigpdvapi/DatasigPdvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.datasig.datasigpdvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasigPdvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
